be5cbe99b6655a082efe567caf9298c832708bbf
Allen Gabrielle Cruiz <cruizallen2@gmail.com>
2025-12-04 10:54:30 +0800

    Refactor servo control script for clarity and functionality

--- scripts/servo.py
+++ scripts/servo.py
@@ -1,47 +1,24 @@
-# Import libraries
+# We imports the GPIO module
 import RPi.GPIO as GPIO
-import time
+# We import the command sleep from time
+from time import sleep
 
-# Set GPIO numbering mode
+# Stops all warnings from appearing
+GPIO.setwarnings(False)
+
+# We name all the pins on BOARD mode
 GPIO.setmode(GPIO.BOARD)
+# Set an output for the PWM Signal
+GPIO.setup(11, GPIO.OUT)
 
-# Set pin 11 as an output, and set servo1 as pin 11 as PWM
-GPIO.setup(11,GPIO.OUT)
-servo1 = GPIO.PWM(11,50) # Note 11 is pin, 50 = 50Hz pulse
-
-#start PWM running, but with value of 0 (pulse off)
-servo1.start(0)
-print ("Waiting for 2 seconds")
-time.sleep(2)
-
-#Let's move the servo!
-print ("Rotating 180 degrees in 10 steps")
-
-# Define variable duty
-duty = 2
-
-# Loop for duty values from 2 to 12 (0 to 180 degrees)
-while duty <= 12:
-    servo1.ChangeDutyCycle(duty)
-    time.sleep(1)
-    duty = duty + 1
-
-# Wait a couple of seconds
-time.sleep(2)
-
-# Turn back to 90 degrees
-print ("Turning back to 90 degrees for 2 seconds")
-servo1.ChangeDutyCycle(7)
-time.sleep(2)
-
-#turn back to 0 degrees
-print ("Turning back to 0 degrees")
-servo1.ChangeDutyCycle(2)
-time.sleep(0.5)
-servo1.ChangeDutyCycle(0)
-
-#Clean things up at the end
-servo1.stop()
-GPIO.cleanup()
-print ("Goodbye")
-
+# Set up the PWM on pin #11 at 50Hz
+pwm = GPIO.PWM(11, 50)
+pwm.start(0) # Start the servo with 0 duty cycle ( at 0 deg position )
+pwm.ChangeDutyCycle(5) # Tells the servo to turn to the left ( -90 deg position )
+sleep(0.5) # Tells the servo to Delay for 5sec
+pwm.ChangeDutyCycle(7.5) # Tells the servo to turn to the neutral position ( at 0 deg position )
+sleep(0.5) # Tells the servo to Delay for 5sec
+pwm.ChangeDutyCycle(10) # Tells the servo to turn to the right ( +90 deg position )
+sleep(0.5) # Tells the servo to Delay for 5sec
+pwm.stop(0) # Stop the servo with 0 duty cycle ( at 0 deg position )
+GPIO.cleanup() # Clean up all the ports we've used.
